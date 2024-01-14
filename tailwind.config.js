b54953cf2af40b5e9e7b62cbe2f6d78dcb7fdae4
Lílian Saraiva <lili.ggsb@gmail.com>
2024-01-14 00:31:12 -0400

    navbar v1.0

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        customColor: '#E0953F', 
+      },
+    },
+  },
+  plugins: [],
+}
+
