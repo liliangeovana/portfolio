ba4d0429b9690cedbdfbc02d5526c0e599907902
Lílian Saraiva <lili.ggsb@gmail.com>
2024-01-15 16:47:25 -0400

    projetos

--- public/script.js
+++ public/script.js
@@ -7,3 +7,16 @@ document.getElementById('close-menu').addEventListener('click', function () {
     var mobileMenu = document.getElementById('mobile-menu');
     mobileMenu.classList.add('hidden');
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var sobreMimLink = document.getElementById('sobreMimLink');
+    var aboutMeDiv = document.querySelector('.aboutMe');
+
+    sobreMimLink.addEventListener('click', function() {
+        aboutMeDiv.classList.add('piscarBorda');
+
+        setTimeout(function() {
+            aboutMeDiv.classList.remove('piscarBorda');
+        }, 500);
+    });
+});
